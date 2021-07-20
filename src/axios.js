9806f4955bf6843b51cf16566a7efbb7b636a7e8
AbisuwaOluwadunsin <ayodejiabisuwa23@gmail.com>
2021-07-21 00:06:06 +0100

    ignore

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //base URL to make requests to the movie database
 const instance = axios.create({
-    baseURL: "https://api.themoviedb.org/3",
+    baseURL: "",
 
 });
 
